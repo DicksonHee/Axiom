4415baacc33dc7196e1f184750412cf0b54bdf98
DHee-Home <1023524@student.sae.edu.au>
2022-07-10 13:59:15 +0800

    Main Menu

---
+++ Assets/_Project/2-Scripts/2-UI/MainMenu/AxiomAnimator.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class AxiomAnimator : MonoBehaviour
+{
+    public GameObject aLetter;
+    public GameObject xLetter;
+    public GameObject iLetter;
+    public GameObject oLetter;
+    public GameObject mLetter;
+
+    private void Awake()
+    {
+        TweenParams tParms = new TweenParams().SetLoops(-1, LoopType.Incremental).SetEase(Ease.InFlash).SetEase(Ease.OutQuad);
+        aLetter.transform.DOLocalRotate(new Vector3(0f, -180f, 0f), 5f, RotateMode.FastBeyond360).SetAs(tParms);
+        xLetter.transform.DOLocalRotate(new Vector3(360f, 0f, 0f), 5f, RotateMode.LocalAxisAdd).SetAs(tParms);
+        iLetter.transform.DOLocalRotate(new Vector3(360f, 0f, 0f), 5f, RotateMode.LocalAxisAdd).SetAs(tParms);
+        oLetter.transform.DOLocalRotate(new Vector3(0f, 0f, 360f), 5f, RotateMode.LocalAxisAdd).SetAs(tParms);
+        mLetter.transform.DOLocalRotate(new Vector3(0f, 360f, 0f), 5f, RotateMode.LocalAxisAdd).SetAs(tParms);
+    }
+
+    public void StopAnim()
+    {
+        aLetter.transform.DOKill();
+        xLetter.transform.DOKill();
+        iLetter.transform.DOKill();
+        oLetter.transform.DOKill();
+        mLetter.transform.DOKill();
+        
+        TweenParams tParms = new TweenParams().SetEase(Ease.InExpo).SetEase(Ease.OutBack);
+        aLetter.transform.DOLocalRotate(new Vector3(0f, 0f, 0f), 2f).SetAs(tParms);
+        xLetter.transform.DOLocalRotate(new Vector3(0f, 0f, 0f), 2f).SetAs(tParms);
+        iLetter.transform.DOLocalRotate(new Vector3(0f, 0f, 0f), 2f).SetAs(tParms);
+        oLetter.transform.DOLocalRotate(new Vector3(0f, 0f, 0f), 2f).SetAs(tParms);
+        mLetter.transform.DOLocalRotate(new Vector3(0f, 0f, 0f), 2f).SetAs(tParms);
+    }
+
+    public void ZoomLogo()
+    {
+        StartCoroutine(ZoomLogo_CO());
+    }
+
+    private IEnumerator ZoomLogo_CO()
+    {
+        aLetter.transform.DOKill();
+        xLetter.transform.DOKill();
+        iLetter.transform.DOKill();
+        oLetter.transform.DOKill();
+        mLetter.transform.DOKill();
+
+        TweenParams tParms = new TweenParams().SetEase(Ease.InExpo);
+        aLetter.transform.DOMoveZ(-50f, 1f).SetAs(tParms);
+        yield return new WaitForSeconds(0.1f);
+        xLetter.transform.DOMoveZ(-50f, 1f).SetAs(tParms);
+        yield return new WaitForSeconds(0.1f);
+        iLetter.transform.DOMoveZ(-50f, 1f).SetAs(tParms);
+        yield return new WaitForSeconds(0.1f);
+        oLetter.transform.DOMoveZ(-50f, 1f).SetAs(tParms);
+        yield return new WaitForSeconds(0.1f);
+        mLetter.transform.DOMoveZ(-50f, 1f).SetAs(tParms);
+    }
+}
--- Assets/_Project/2-Scripts/2-UI/MainMenu/MainMenuAnim.cs
+++ Assets/_Project/2-Scripts/2-UI/MainMenu/MainMenuAnim.cs
@@ -2,51 +2,124 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using Unity.Mathematics;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class MainMenuAnim : MonoBehaviour
 {
-    public Transform centerPoint;
-    public float radius;
-    public int spawnAmount;
-    public float rotationSpeed;
-    public ObjectAnimMode mode;
-    public AnimationCurve curve;
+    public Transform spawnParent;
     
+    public float radius = 5;
+    public int spawnAmount = 100;
+    public bool clockwiseRotation = true;
+    public float rotationDuration = 5f;
+    public Vector3 rotationDir;
+    
+    public ObjectAnimMode mode;
     public GameObject spawnObject;
+    public Color redCol;
+    public Color blueCol;
 
     private List<GameObject> spawnedObjectsList = new();
     private float currentDegrees;
-
+    private Vector3 initialCenterPoint;
+    private Vector3 currentCenterPoint;
+    
     private void Awake()
     {
+        initialCenterPoint = transform.position;
+        currentCenterPoint = initialCenterPoint;
+        
         for (int ii = 0; ii < spawnAmount; ii++)
         {
-            spawnedObjectsList.Add(Instantiate(spawnObject));
+            float currentRad = ii * (360f/spawnAmount) * Mathf.Deg2Rad;
+            GameObject spawnedObject = Instantiate(spawnObject, spawnParent.transform);
+            spawnedObject.transform.position = currentCenterPoint + new Vector3(Mathf.Sin(currentRad), Mathf.Cos(currentRad)) * radius;
+            spawnedObjectsList.Add(spawnedObject);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        ShapeRotation();
-        
-        if(Input.GetKeyDown(KeyCode.Space)) ObjectRotation_CO(new Vector3(360f, 0,360f), 2f, mode);
-
-        currentDegrees += Time.deltaTime * rotationSpeed;
-    }
-
-    private void ShapeRotation()
-    {
         for (int ii = 0; ii < spawnedObjectsList.Count; ii++)
         {
-            float currentRad = (ii * (360f/spawnedObjectsList.Count) + currentDegrees) * Mathf.Deg2Rad;
-            spawnedObjectsList[ii].transform.position = centerPoint.position + new Vector3(Mathf.Sin(currentRad), Mathf.Cos(currentRad)) * (radius);
+            float currentRad = (ii * (360f/spawnAmount) + Time.time * rotationDuration) * Mathf.Deg2Rad;
+            float xVal = Mathf.Sin(currentRad);
+            float yVal = Mathf.Cos(currentRad);
+            if (xVal == 0) xVal = 0.001f;
+            float zVal = Mathf.Tan(yVal/xVal);
+            spawnedObjectsList[ii].transform.position = currentCenterPoint + new Vector3(xVal, yVal, zVal) * radius;
         }
     }
 
-    private void ObjectRotation_CO(Vector3 rotation, float duration, ObjectAnimMode animMode)
+    public void ChangeColor()
+    {
+        foreach (var t in spawnedObjectsList)
+        {
+            Color targetCol = Random.Range(0, 2) == 0 ? redCol : blueCol;
+            t.GetComponent<MeshRenderer>().material.DOColor(targetCol, Random.Range(0.5f, 1f));
+            t.transform.DOLocalRotate(new Vector3(0, 0, 360), 2f, RotateMode.LocalAxisAdd).SetLoops(-1);
+        }
+    }
+
+    public void ChangeCenter(Vector3 newCenter, float newRadius)
+    {
+        foreach (GameObject obj in spawnedObjectsList) obj.transform.DOKill(true);
+        transform.DOKill(true);
+        spawnParent.DOKill(true);
+        
+        List<int> randList = new();
+        for(int ii = 0; ii < spawnedObjectsList.Count; ii++) randList.Add(ii);
+
+        for (int ii = 0; ii < randList.Count; ii++)
+        {
+            int temp = randList[ii];
+            int randomIndex = Random.Range(ii, spawnedObjectsList.Count);
+            randList[ii] = randList[randomIndex];
+            randList[randomIndex] = temp;
+        }
+        
+        for (int ii = 0; ii < randList.Count; ii++)
+        {
+            float currentRad = randList[ii] * (360f/spawnAmount) * Mathf.Deg2Rad;
+            spawnedObjectsList[ii].transform.DOMove(newCenter + new Vector3(Mathf.Sin(currentRad), Mathf.Cos(currentRad)) * newRadius, 2f).SetEase(Ease.InOutQuad);
+        }
+    }
+
+    public void ResetCenter()
+    {
+        foreach (GameObject obj in spawnedObjectsList) obj.transform.DOKill(true);
+        transform.DOKill(true);
+        spawnParent.DOKill(true);
+        
+        for (int ii = 0; ii < spawnedObjectsList.Count; ii++)
+        {
+            float currentRad = ii * (360f/spawnAmount) * Mathf.Deg2Rad;
+            spawnedObjectsList[ii].transform.DOMove(initialCenterPoint + new Vector3(Mathf.Sin(currentRad), Mathf.Cos(currentRad)) * radius, 2f).SetEase(Ease.InOutQuad);
+        }
+        
+        transform.DORotate(rotationDir, 5f, RotateMode.WorldAxisAdd).SetLoops(-1).SetEase(Ease.Linear);
+    }
+
+    private void StopRotation()
+    {
+        foreach (GameObject obj in spawnedObjectsList) obj.transform.DOKill(true);
+        transform.DOKill(true);
+        spawnParent.DOKill(true);
+        
+        for (int ii = 0; ii < spawnedObjectsList.Count; ii++)
+        {
+            float currentRad = ii * (360f/spawnAmount) * Mathf.Deg2Rad;
+            spawnedObjectsList[ii].transform.DOMove(initialCenterPoint + new Vector3(Mathf.Sin(currentRad), Mathf.Cos(currentRad)) * radius, 2f).SetEase(Ease.InExpo);
+        }
+        float zDir = clockwiseRotation ? 360f : -360f;
+        transform.DORotate(Vector3.zero, 1f, RotateMode.FastBeyond360).SetEase(Ease.Linear);
+        spawnParent.DOLocalRotate(new Vector3(0f, 0, zDir), rotationDuration, RotateMode.LocalAxisAdd).SetLoops(-1).SetEase(Ease.Linear);
+    }
+
+    public void ObjectRotation(Vector3 rotation, float duration, ObjectAnimMode animMode)
     {
         switch (animMode)
         {
@@ -110,9 +183,9 @@ public class MainMenuAnim : MonoBehaviour
         }
     }
 
-    private void ObjectRotation_Random(Vector3 rotation, float duration) => StartCoroutine(ORRandom_CO(rotation, duration));
+    private void ObjectRotation_Random(Vector3 rotation, float duration) => ORRandom_CO(rotation, duration);
     
-    private IEnumerator ORRandom_CO(Vector3 rotation, float duration)
+    private void ORRandom_CO(Vector3 rotation, float duration)
     {
         List<int> randList = new();
         for(int ii = 0; ii < spawnedObjectsList.Count; ii++) randList.Add(ii);
@@ -127,7 +200,6 @@ public class MainMenuAnim : MonoBehaviour
 
         foreach (int num in randList)
         {
-            yield return new WaitForSeconds(0.01f);
             spawnedObjectsList[num].transform.DORotate(rotation, duration, RotateMode.FastBeyond360);
         }
     }
