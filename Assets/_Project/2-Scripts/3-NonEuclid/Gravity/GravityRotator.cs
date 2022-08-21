ca54f0ea8311ef18eda1d886ff79f5f6a3b7a5ce
DHee-Home <1023524@student.sae.edu.au>
2022-08-21 16:19:21 +0800

    Gravity stuff

--- Assets/_Project/2-Scripts/3-NonEuclid/Gravity/GravityRotator.cs
+++ Assets/_Project/2-Scripts/3-NonEuclid/Gravity/GravityRotator.cs
@@ -1,41 +1,27 @@
 using System;
 using UnityEngine;
 using Axiom.Core;
+using DG.Tweening;
 
 namespace Axiom.NonEuclidean
 {
     public class GravityRotator : MonoBehaviour
     {
-        private bool isRotating;
-
-        private void Awake()
-        {
-            InvokeRepeating(nameof(CheckPerfectRotation), 0f, 0.5f);
-        }
-
-        void LateUpdate()
+        void Update()
         {
             CheckRotation();
         }
 
         private void CheckRotation()
         {
-            isRotating = false;
-            Vector3 rotateFrom = -transform.up.normalized;
-            Vector3 rotateTo = Physics.gravity.normalized;
+            Vector3 rotateFrom = -transform.up;
+            Vector3 rotateTo = Physics.gravity;
             if (Vector3.Dot(rotateFrom, rotateTo) > 0.99f) return;
-            
-            Debug.Log("ASd");
-            isRotating = true;
+
+
             Vector3 cross = Vector3.Cross(rotateFrom, rotateTo);
             float angle = Vector3.SignedAngle(rotateFrom, rotateTo, cross);
             transform.Rotate(cross, angle, Space.World);
-        }
-
-        private void CheckPerfectRotation()
-        {
-            if (isRotating) return;
-            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.RoundToSpecificNumber(90));
         }
     }
 }
