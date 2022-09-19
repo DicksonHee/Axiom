75d10537bf4ca6b1807894be8a5b6f81b221588f
Bil <1020453@student.sae.edu.au>
2022-09-20 01:52:07 +0800

    Fmod Footstep implementation

---
+++ Assets/_Project/2-Scripts/4-Misc/Footstep.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+using UnityEngine.Serialization;
+
+
+public class Footstep : MonoBehaviour
+{
+    [SerializeField]
+    private StudioEventEmitter eventEmmitter;
+    public EventReference footstepEvent;
+
+    [ParamRef]
+    [FormerlySerializedAs("parameter")]
+    public string globalSurface;
+
+    [FormerlySerializedAs("value")]
+    public int Value; //currently 0-7 changes instrument
+
+    [SerializeField]
+    private PARAMETER_DESCRIPTION parameterDescription;
+    public PARAMETER_DESCRIPTION ParameterDesctription { get { return parameterDescription; } }
+
+    FMOD.RESULT currentValue;
+    float currentValuef;
+
+    [SerializeField]
+    private string labelName;
+
+   
+    private FMOD.RESULT Lookup()
+    {
+        FMOD.RESULT result = RuntimeManager.StudioSystem.getParameterDescriptionByName(globalSurface, out parameterDescription);
+        return result;
+    }
+
+    private void Awake()
+    {
+        if (string.IsNullOrEmpty(parameterDescription.name))
+        {
+            Lookup();
+        }
+        //set string once
+        try
+        {
+            RuntimeManager.StudioSystem.getParameterLabelByID(parameterDescription.id, (int)currentValuef, out labelName);
+        }
+        catch(System.Exception ex)
+        {
+            Debug.Log(ex);
+        }
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha0))
+        {
+           eventEmmitter.Play();
+        }
+        currentValue  = RuntimeManager.StudioSystem.getParameterByID(parameterDescription.id, out currentValuef);
+        RuntimeManager.StudioSystem.getParameterLabelByID(parameterDescription.id, (int)currentValuef, out labelName);
+
+        if(currentValuef == Value) return;
+        FMOD.RESULT result = RuntimeManager.StudioSystem.setParameterByID(parameterDescription.id, Value);
+       
+        if (result != FMOD.RESULT.OK)
+        {
+            RuntimeUtils.DebugLogError(string.Format(("[FMOD] StudioGlobalParameterTrigger failed to set parameter {0} : result = {1}"), globalSurface, result));
+        }
+    }
+}
