21d19c10bb1292dba3a212f58dbd3cf43116dd5a
Rhys <1022076@student.sae.edu.au>
2022-11-16 11:50:19 +0800

    Updates

--- Assets/_Project/2-Scripts/4-Misc/Spawner.cs
+++ Assets/_Project/2-Scripts/4-Misc/Spawner.cs
@@ -8,12 +8,13 @@ public class Spawner : MonoBehaviour
     [SerializeField] private Transform spawnPos;
     [SerializeField] private Transform spawnDest;
     [SerializeField] private float speed;
+	
 	private void OnTriggerEnter(Collider other)
 	{
 		if(other.tag == "Player")
 		{
 			var clone = Instantiate(spawnObject, spawnPos);
-			clone.GetComponent<Rigidbody>().AddForce(spawnDest.transform.position * speed);
+			clone.GetComponent<Rigidbody>().AddForce(spawnDest.position - spawnPos.position * speed, ForceMode.Impulse);
 			Destroy(clone, 10);
 			gameObject.GetComponent<Collider>().enabled = false;
 		}
