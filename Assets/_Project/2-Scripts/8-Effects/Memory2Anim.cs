553141a79af04ce91ceeef7f579cee6ee900369c
Aubrey <aubreyvigus@gmail.com>
2022-11-24 20:41:10 +0800

    hi uwu

---
+++ Assets/_Project/2-Scripts/8-Effects/Memory2Anim.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Memory2Anim : MonoBehaviour
+{
+    public float animTime;
+    public FadeMaterial[] fadeMaterials;
+    public GameObject[] inObjects, outObjects;
+
+    private const float opaque = 3.5f;
+    private const float transparent = 60f;
+
+    private bool hasActivated = false;
+
+    private void Awake()
+    {
+        SetMaterialTransparency(0);
+    }
+
+    public void PlayAnim() 
+    {
+        if (hasActivated) return;
+
+        StartCoroutine(Anim());
+        hasActivated = true;
+    }
+
+    private void SetMaterialTransparency(float t)
+    {
+        foreach (var mat in fadeMaterials)
+            mat.SetTransparency(t);
+    }
+
+    private IEnumerator Anim()
+    {
+        foreach (var obj in inObjects)
+            obj.SetActive(true);
+
+        for (float elapsed = 0f; elapsed < animTime; elapsed += Time.deltaTime)
+        {
+            float t = elapsed / animTime;
+            SetMaterialTransparency(t);
+            yield return null;
+        }
+
+        SetMaterialTransparency(1);
+
+        foreach (var obj in outObjects)
+            obj.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        SetMaterialTransparency(0);
+    }
+
+    [System.Serializable]
+    public class FadeMaterial
+    {
+        public Material material;
+        public Vector2 transparencyRange;
+
+        public void SetTransparency(float t) => 
+            material.SetFloat("_Transparency", Mathf.Lerp(transparencyRange.x, transparencyRange.y, t));
+    }
+}
