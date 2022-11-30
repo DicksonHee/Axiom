5332ebf27b59abdf6fe5ac2861052b19315ae419
DHee-SAE <1023524@student.sae.edu.au>
2022-11-30 18:10:06 +0800

    Apartment items done

--- Assets/_Project/2-Scripts/2-UI/InteractUI.cs
+++ Assets/_Project/2-Scripts/2-UI/InteractUI.cs
@@ -49,12 +49,16 @@ public class InteractUI : MonoBehaviour
         objectName.text = objectSO.objectName;
         objectDescription.text = objectSO.objectDescription;
         interactionTextCanvasGroup.alpha = 0f;
+        interactionDetailsCanvasGroup.blocksRaycasts = true;
+        interactionDetailsCanvasGroup.interactable = true;
         interactionDetailsCanvasGroup.alpha = 1f;
     }
 
     private void HideDetailsCanvas()
     {
         interactionTextCanvasGroup.alpha = 1f;
+        interactionDetailsCanvasGroup.blocksRaycasts = false;
+        interactionDetailsCanvasGroup.interactable = false;
         interactionDetailsCanvasGroup.alpha = 0f;
     }
 }
