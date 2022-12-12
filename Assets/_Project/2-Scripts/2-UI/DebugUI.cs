f681059eb2dc84d3b1f7e771f09d11ce36087cb9
DHee-SAE <1023524@student.sae.edu.au>
2022-12-12 18:56:31 +0800

    Increased main menu resolution, Replaced all player prefabs

--- Assets/_Project/2-Scripts/2-UI/DebugUI.cs
+++ Assets/_Project/2-Scripts/2-UI/DebugUI.cs
@@ -1,25 +1,47 @@
 using TMPro;
 using UnityEngine;
 using Axiom.Player.Movement.StateMachine;
+using UnityEngine.SceneManagement;
 
 namespace Axiom.UI
 {
     public class DebugUI : MonoBehaviour
     {
-        public MovementSystem _movementSystem;
+        private MovementSystem movementSystem;
 
         public TMP_Text _currentTimer;
         public TMP_Text _currentSpeed;
         public TMP_Text _currentState;
         public TMP_Text _previousState;
 
+        private void OnEnable()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnDisable()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            GetPlayerMovementSystem();
+        }
+
         private void Update()
         {
-            _currentSpeed.text = "Current Speed: " + _movementSystem.GetCurrentSpeed();
-            _currentState.text = "Current State: " + _movementSystem.GetCurrentStateName();
-            _previousState.text = "Previous State: " + _movementSystem.GetPreviousStatename();
+            if (movementSystem == null) return;
+            _currentSpeed.text = "Current Speed: " + movementSystem.GetCurrentSpeed();
+            _currentState.text = "Current State: " + movementSystem.GetCurrentStateName();
+            _previousState.text = "Previous State: " + movementSystem.GetPreviousStatename();
 
             //if(SpeedrunTimer._isActive) _currentTimer.text = "" + Mathf.Round(SpeedrunTimer.GetElapsedTime() * 100f)/100f;
+        }
+
+        private void GetPlayerMovementSystem()
+        {
+            movementSystem = FindObjectOfType<MovementSystem>();
         }
     }
 }
