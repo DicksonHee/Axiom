b2f9c840268b1ea56c35dd6719c7ab0da4fe9d31
Aubrey <aubreyvigus@gmail.com>
2022-12-14 13:27:16 +0800

    miles jump out of alex brain

--- Assets/_Project/2-Scripts/4-Misc/MindEscape.cs
+++ Assets/_Project/2-Scripts/4-Misc/MindEscape.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Bezier;
+using UnityEngine.SceneManagement;
 
 public class MindEscape : MonoBehaviour
 {
     public Animator anim;
 
+    public string sceneToLoad;
+
     public Curve jumpCurve;
     public float playerStartT, coreStartT;
     public float coreTDistance;
 
-    public Transform core;
+    public Transform core, landSpot;
     public Renderer coreRenderer;
     public Light coreLight;
+    public float officeScale;
 
     private Transform player;
     private Camera cam;
@@ -29,6 +33,7 @@ public class MindEscape : MonoBehaviour
         SetInputActive(false);
         SetAnimVariables();
         anim.SetTrigger("Jump");
+        StartCoroutine(ILoadScene());
     }
 
     private void Update()
@@ -46,15 +51,20 @@ public class MindEscape : MonoBehaviour
         Axiom.Core.PlayerMovementDetails.cameraLookEnabled = active;
     }
 
-    private Vector3 camForward1;
+    private Vector3 camForward1, camForward2;
     private float playerEndT;
     private float lightIntensity1;
+    private float coreScale1, coreScale2;
     private void SetAnimVariables()
     {
         camForward1 = cam.transform.forward;
+        camForward2 = jumpCurve.GetDerivativeNormalised(1f).normalized;
         playerEndT = 1f - coreTDistance;
 
         lightIntensity1 = coreLight.intensity;
+
+        coreScale1 = core.transform.localScale.x;
+        coreScale2 = coreScale1 * (1f / officeScale);
     }
 
     [Range(0, 1)] public float pathProgress, pathCloseness;
@@ -64,15 +74,35 @@ public class MindEscape : MonoBehaviour
     private void UpdateAnimVariables()
     {
         float t = Mathf.Lerp(playerStartT, playerEndT, pathProgress);
-        player.position = jumpCurve.GetCurvePointNormalised(t) + curveOffset * (1f - pathCloseness);
-        Vector3 camForward2 = jumpCurve.GetDerivativeNormalised(t);
+
+        Vector3 curvePosition = jumpCurve.GetCurvePointNormalised(t) + curveOffset * (1f - pathCloseness);
+        player.position = Vector3.Lerp(curvePosition, landSpot.position, coreZoom);
         cam.transform.forward = Vector3.Lerp(camForward1, camForward2, cameraLook);
 
-        float coreT = Mathf.Max(t + coreTDistance * (1f - coreZoom), coreStartT);
+        float coreT = Mathf.Max(t + coreTDistance, coreStartT); // coreTDistance * (1f - coreZoom)
         core.position = jumpCurve.GetCurvePointNormalised(coreT);
+        core.localScale = Vector3.one * Mathf.Lerp(coreScale1, coreScale2, coreZoom);
 
         coreLight.intensity = Mathf.Lerp(lightIntensity1, 0f, coreLightFade);
         coreRenderer.material.SetColor("_BaseColor", new Color(1, 1, 1, 1f - officeFade));
+    }
+
+    public void LoadScene() => canLoad = true;
+
+    private bool canLoad = false;
+    private IEnumerator ILoadScene()
+    {
+        if (!SceneManager.GetSceneByName("Load_Scene").isLoaded) SceneManager.LoadSceneAsync("Load_Scene", LoadSceneMode.Additive);
+        while (!SceneManager.GetSceneByName("Load_Scene").isLoaded)
+        {
+            yield return null;
+        }
+
+        yield return new WaitUntil(() => canLoad);
+
+        LoadScreen.current.SetBlack();
+        LoadScreen.current.SetOpaque();
+        SceneLoad_Manager.LoadSpecificScene(sceneToLoad);
     }
 
     //private void OnDrawGizmos()
